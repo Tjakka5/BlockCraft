30fd899387f90b6b794a27bc9ca8b93082657419
Justin van der Leij <justinvanderleij@gmail.com>
2018-12-06 21:35:41 +0100

    Set up tilemap. Set up grass ruletile

--- Game_BlockCraft/Assets/Scripts/PlayerController.cs
+++ Game_BlockCraft/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,26 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
 	[SerializeField]
 	private float speed = 16.0f;
 
-	void Update()
+	private Rigidbody2D rb = null;
+
+	public void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	public void Update()
 	{
 		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
 		Vector2 movement = input.normalized * speed;
 		Vector2 deltaMovement = movement * Time.deltaTime;
 
-		transform.position += new Vector3(deltaMovement.x, deltaMovement.y);
+		//transform.position += new Vector3(deltaMovement.x, deltaMovement.y);
+		rb.AddForce(deltaMovement);
 	}
 }
