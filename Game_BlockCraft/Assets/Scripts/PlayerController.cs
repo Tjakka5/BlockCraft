b533fbcb08a482f35465364ea2d7953803954095
Justin van der Leij <justinvanderleij@gmail.com>
2018-12-06 21:07:58 +0100

    Set up player with cinemachine

---
+++ Game_BlockCraft/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	private float speed = 16.0f;
+
+	void Update()
+	{
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		Vector2 movement = input.normalized * speed;
+		Vector2 deltaMovement = movement * Time.deltaTime;
+
+		transform.position += new Vector3(deltaMovement.x, deltaMovement.y);
+	}
+}
